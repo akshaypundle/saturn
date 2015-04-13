c03e65d3806ca70fc995d9a7344710a4628c741e
Akshay Pundle <apundle@palantir.com>
2015-04-13 00:22:11 -0700

    add config dir param to monitoring script

--- scripts/monitoring.sh
+++ scripts/monitoring.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+if [ "$#" -ne 1 ]; then
+    echo "Usage: monitoring.sh <path of web root>"
+    exit
+fi
+
 echo "" > /tmp/aa;
 curl -m 20  http://localhost/ > /tmp/aa;
 actualsize=$(wc -c /tmp/aa | awk {'print $1'})
@@ -6,6 +11,6 @@ if [ $actualsize -ge 100 ]; then
   echo "ok"
 else
   killall python;  
-  cd /home/ec2-user/webserver &&  nohup python -m SimpleHTTPServer 80
+  cd $1 &&  nohup python -m SimpleHTTPServer 80
 fi
 
