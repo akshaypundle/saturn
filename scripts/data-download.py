4e456f73d52b98aff12c7881906e4ab02a499965
Akshay Pundle <apundle@palantir.com>
2015-05-29 15:00:23 -0700

    Start travis support
      - commit tmp changes, move back to java 1.7 since amazon does have 1.8
        out of the box.
      - add .travis.yml

--- scripts/data-download.py
+++ scripts/data-download.py
@@ -4,14 +4,21 @@ import urllib2
 import re
 import os.path
 import time
+import sys
 
 from sets import Set
 from stripogram import html2text
+import pandas as pd
 from pandas.io.data import Options, RemoteDataError
 
+if len(sys.argv) != 2:
+    print "Usage: data-download.py <base dir>"
+    sys.exit(1)
+    
+baseDir = sys.argv[1]+"/"
 blankLine = re.compile("^\s*$");
 ignoreLine = re.compile("^\s*#");
-tickersFile = open("tickers")
+tickersFile = open(baseDir+"/tickers")
 tickersList = []
 startTickerIndex = 0
 downloaded = set()
@@ -22,7 +29,7 @@ for line in tickersFile:
     tickersList.append(ticker);
 
 if os.path.isfile("last-downloaded"):
-  last = open("last-downloaded")
+  last = open(baseDir+"last-downloaded")
   lastTicker = last.readline().strip()
   try:
     startTickerIndex = tickersList.index(lastTicker) + 1
@@ -45,13 +52,14 @@ for i in range(startTickerIndex, len(tickersList)):
   toDownload = Options(ticker, "yahoo")
   try:
     data = toDownload.get_options_data();
-    data.to_csv("data/" + ticker)
+    pd.DataFrame(data).to_csv(baseDir+"/data/" + ticker)
     print "done."
   except (ValueError, RemoteDataError,  StopIteration) as e:
     print "skipped."
+    print e
 
   downloaded.add(ticker)
-  last = open("last-downloaded", "w")
+  last = open(baseDir+"last-downloaded", "w")
   if i != len(tickersList) - 1:
     last.write(ticker)
   last.close()
